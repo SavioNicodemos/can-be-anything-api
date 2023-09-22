ce7346fa2656fb73711c1381e3fbe472e8dde982
SavioNicodemos <nicodemosgcosta@gmail.com>
2023-09-22 01:44:36 +0100

    Improve ApiResponser

--- app/Traits/ApiResponser.php
+++ app/Traits/ApiResponser.php
@@ -3,22 +3,50 @@
 namespace App\Traits;
 
 use Illuminate\Http\JsonResponse;
+use Symfony\Component\HttpFoundation\Response;
 
 trait ApiResponser
 {
     /**
      * Build success response
      */
-    public function successResponse(mixed $data, int $statusCode = 200): JsonResponse
+    public function successResponse(mixed $data, int $code = Response::HTTP_OK, string $message = ''): JsonResponse
     {
-        return response()->json($data, $statusCode);
+        return $this->buildResponse(
+            success: true,
+            data: $data,
+            code: $code,
+            message: $message,
+        );
+    }
+
+    private function buildResponse(
+        bool   $success = true,
+        mixed  $data = null,
+        int    $code = Response::HTTP_OK,
+        string $message = ''
+    ): JsonResponse
+    {
+        return response()->json(
+            [
+                'success' => $success,
+                'message' => $message,
+                'data' => $data,
+            ],
+            $code
+        );
     }
 
     /**
      * Build error response
      */
-    public function errorResponse(string $message, int $statusCode = 400): JsonResponse
+    public function errorResponse(string $message, int $code = 400): JsonResponse
     {
-        return response()->json(['message' => $message], $statusCode);
+        return $this->buildResponse(
+            success: false,
+            data: null,
+            code: $code,
+            message: $message,
+        );
     }
 }
